024013ed19be96dc4ebb06ea7bb72ecc45045f23
Ryan Booh <macuser@Macs-MacBook-Pro.local>
2020-05-28 06:57:24 -0400

    Updates

---
+++ precache-manifest.0afdb0597cbf513bb1d22353fc517d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93dd12ee0b74efbe8302e46ec12fcd1",
+    "url": "/hacker-news-api-search/index.html"
+  },
+  {
+    "revision": "42466c9b5189e1794492",
+    "url": "/hacker-news-api-search/static/css/main.4d7d0dad.chunk.css"
+  },
+  {
+    "revision": "c224300ad45760d6ce3e",
+    "url": "/hacker-news-api-search/static/js/2.5c2fcf33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news-api-search/static/js/2.5c2fcf33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42466c9b5189e1794492",
+    "url": "/hacker-news-api-search/static/js/main.0e431863.chunk.js"
+  },
+  {
+    "revision": "ad9a99eccaf28aaf73b4",
+    "url": "/hacker-news-api-search/static/js/runtime-main.b542e070.js"
+  }
+]);
